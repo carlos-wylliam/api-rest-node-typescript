4ac74719373304f61d3bc65682dbe1b00b84fb58
carlos-wylliam <carloswylliam023@gmail.com>
2024-02-21 18:58:23 -0300

    feat: added yup validation to cidades creates controller

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { CidadesController } from "./../controllers";
+
 const router = Router();
 
 router.get("/", (_, res) => {
   return res.status(StatusCodes.OK).send("Olá, Dev!")
 });
+
+router.post("/cidades", CidadesController.create)
 
 export { router };
