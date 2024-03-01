d0375a374d619af0acf803197d65ab6553a2cd18
carlos-wylliam <carloswylliam023@gmail.com>
2024-02-29 21:48:43 -0300

    added file build

---
+++ build/server/routes/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+const http_status_codes_1 = require("http-status-codes");
+const controllers_1 = require("./../controllers");
+const router = (0, express_1.Router)();
+exports.router = router;
+router.get("/", (_, res) => {
+    return res.status(http_status_codes_1.StatusCodes.OK).send("Olá, Dev!");
+});
+router.get("/cidades", controllers_1.CidadesController.getAllValidation, controllers_1.CidadesController.getAll);
+router.post("/cidades", controllers_1.CidadesController.createValidation, controllers_1.CidadesController.create);
+router.get("/cidades/:id", controllers_1.CidadesController.getByIdValidation, controllers_1.CidadesController.getById);
+router.put("/cidades/:id", controllers_1.CidadesController.updateByIdValidation, controllers_1.CidadesController.updateById);
+router.delete("/cidades/:id", controllers_1.CidadesController.deleteByIdValidation, controllers_1.CidadesController.deleteById);
