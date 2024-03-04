9d4f9573e3b173f1bd0c17444dd46006dafef760
carlos-wylliam <carloswylliam023@gmail.com>
2024-03-04 19:47:14 -0300

    feat: added migration to cidade table

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,6 +1,6 @@
 import supertest from 'supertest';
 
-import { server } from '../src/server/Server';
+import { server } from '../src/server/ServerIndex';
 
 
 export const testServer = supertest(server);
